093a394cd6904eeb7769b2e30fce5780bb0f42ca
Fatulallah_Mohammedi <fatullah.mohammedi@docdoc.com>
2023-01-11 18:03:30 +0530

    First commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HtmlComponent msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HtmlComponent from '@/components/HtmlComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    HtmlComponent,
+  },
 }
 </script>
